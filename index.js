1efc61d042e039868ff0ff6e816260204128cfab
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-28 17:29:53 +0300

    Implement the mock history API

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Express = require('express')
 const Morgan = require('morgan')
+const Store = require('./devassets/pkgs.json')
+const Faker = require('faker')
+const Moment = require('moment')
 
 
 const app = Express()
@@ -14,17 +17,104 @@ app.use(bodyLogger())
 
 
 app.get('/_all_docs', (req, res) => {
-  return res.sendStatus(501)
+  const rows = Store.map(pkg => {
+    const row = {
+      id: pkg._id,
+      key: pkg.name,
+      value: {
+        rev: pkg._rev
+      }
+    }
+
+    if (['true', '1'].includes(req.query.include_docs)) {
+      row.doc = pkg
+    }
+
+    return row
+  })
+
+  const total_rows = Store.length
+
+
+  return res.json({
+    total_rows,
+    offset: 0,
+    rows
+  })
 })
 
 
 app.get('/:pkg_name', (req, res) => {
-  return res.sendStatus(501)
+  const doc = Store.find(pkg => req.params.pkg_name === pkg._id)
+
+  if (!doc) {
+    return res.status(404).json({
+      error: 'not_found',
+      reason: 'missing'
+    })
+  }
+
+  return res.json(doc)
+})
+
+
+app.get('/downloads/range/:interval/:pkg_name', (req, res) => {
+  const { interval } = req.params
+
+  if (!interval.includes(':')) {
+    return res.sendStatus(400)
+  }
+
+
+  const [start, end] = interval.split(':')
+  const isValidDate = mdate => !Number.isNaN(mdate.toDate().getTime())
+
+
+  const start_moment = Moment(start)
+
+  if (!isValidDate(start_moment)) {
+    return res.sendStatus(400)
+  }
+
+
+  const end_moment = Moment(end)
+
+  if (!isValidDate(end_moment)) {
+    return res.sendStatus(400)
+  }
+
+
+  const history = []
+  const num_days = end_moment.diff(start_moment, 'days')
+  const day_moment = Moment(start_moment)
+  const formatDate = mdate => mdate.format('yyyy-mm-DD')
+
+  for (let days = 0; days < num_days; days++) {
+    day_moment.add(days, 'days')
+
+    const entry = {
+      downloads: Faker.datatype.number(),
+      day: formatDate(day_moment)
+    }
+
+    history.push(entry)
+  }
+
+
+  const { pkg_name } = req.params
+
+
+  return res.json({
+    "start": start,
+    "end": end,
+    "package": pkg_name,
+    "downloads": history
+  })
 })
 
 
 const server = app.listen(9090, () => {
-  console.log('GitHub API mock server is listening at %j',
+  console.log('npm API mock server is listening at %j',
     server.address())
 })
 
