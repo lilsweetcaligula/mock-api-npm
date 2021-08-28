25295efc81219e770eed5100bd7fc86d9ed6ecd3
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-28 17:29:40 +0300

    Commit useful dev scripts to source control

--- scripts/download_pkgs_of_interest.js
+++ scripts/download_pkgs_of_interest.js
@@ -6,12 +6,18 @@ const REGISTRY_URL = 'https://replicate.npmjs.com'
 app
   .argument('<pkg_names...>')
   .action(async function (pkg_names) {
+    const pkgs = []
+
     for (const pkg_name of pkg_names) {
       const download_url = [REGISTRY_URL, pkg_name].join('/')
       const response = await Axios.get(download_url)
 
-      console.dir(JSON.stringify(response.data), { depth: null })
+      const { data: pkg_data } = response
+      pkgs.push(pkg_data)
     }
+
+
+    process.stdout.write(JSON.stringify(pkgs))
   })
   .parse(process.argv)
 
